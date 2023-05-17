1e3734d75995f4b1e66e30f53feba4c710380a59
ANAN DEESAKUL <tiwyy13@gmail.com>
2023-05-18 00:29:34 +0700

    Ready to use

---
+++ spark/execution_scripts/process.py
@@ -0,0 +1,72 @@
+import os
+import requests
+from operator import add
+from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import monotonically_increasing_id
+from pyspark.sql.functions import concat,col,lit
+
+def download_image(row):
+        photo_path = '/mount_volumn/resources/photos'
+        img_url = row['photo']
+        index = row['index']
+        img_filename = f'{photo_path}/image_{index}.jpg'
+        try: 
+            response = requests.get(img_url)
+            print("Got image from URL")
+        except:
+            print(img_url)
+            print(index)
+        if response.status_code != 200:
+            print("Got not 200")
+            print(response.content)
+            print(response.headers)
+            return
+        try: 
+            with open(img_filename, 'wb') as destination_image:
+                print(f"image name = {img_filename}")
+                destination_image.write(response.content)
+        except:
+             print("IO error")
+
+
+
+spark_url = 'local'
+spark = SparkSession.builder\
+    .master(spark_url)\
+    .appName('Spark')\
+    .getOrCreate()
+
+path = '/mount_volumn/resources/bangkok_traffy.csv'
+print(path)
+df = spark.read.option("delimiter", ",").option("header", True).csv(path)
+cols = [c.replace('.', '_') for c in df.columns]
+df = df.toDF(*cols)
+print("fetched csv")
+
+
+cols = ['coords', 'photo', 'timestamp', 'type']
+df = df.select(cols)
+df = df.filter("type == '{จราจร}'")
+df_filter = df.na.drop()
+df_append_col = df_filter.withColumn('index', monotonically_increasing_id())
+
+
+df_test = df_append_col
+df_test.show()
+df_test.foreach(download_image)
+print("downloading images")
+
+df_filename = df_test.withColumn('filename', concat(lit('image_'), col('index'), lit('.jpg')))
+df_final = df_filename.select(col('coords'), col('timestamp'), col('filename'))
+
+df_final.write.option("header",True).csv('/mount_volumn/resources/processed_files/processed')
+print("Successfully create CSV in processed")
+
+
+
+
+
+
+
+
--- spark/setup.sh
+++
@@ -1,9 +0,0 @@
-!/bin/bash
-
-py -m venv spark
-.\spark\Scripts\activate
-py -m pip install pyspark==3.3.2 request pandas
-
-
-docker-compose up --scale spark-worker=3
-pip install pyspark
